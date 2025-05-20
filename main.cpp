58e7b19e22c034350df5d3665c46aff7947b78b5
AlexNordahl <nordahlalex@gmail.com>
2025-05-20 15:11:17 +0200

    measured time addVertex

--- main.cpp
+++ main.cpp
@@ -1,26 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <string>
 #include "headers/graph.hpp"
 
-int main(int argc, char const *argv[])
+int main() 
 {
-    graph g;
-
-    g.addVertex("A");
-    g.addVertex("B");
-    g.addVertex("C");
-    g.addVertex("D");
+    std::ofstream outfile("vertex_times.txt");
     
-    g.setVertexValue("A", 10);
-    g.setVertexValue("B", 15);
+    const int max_vertices = 10000;
+    const int step = 10;
+    const int repeats = 50;
 
-    g.addEdge("A", "B");
-    g.addEdge("B", "C");
-    g.addEdge("A", "D");
+    for (int current_size = step; current_size <= max_vertices; current_size += step) 
+    {
+        double total_time_us = 0.0;
 
-    g.setEdgeValue("A", "B", 10);
-    g.setEdgeValue("C", "B", 15);
-    g.setEdgeValue("A", "D", 20);
+        for (int i = 0; i < repeats; ++i) {
+            graph g;
 
-    g.printVertexes();
+            for (int j = 1; j < current_size; ++j) 
+            {
+                g.addVertex("V" + std::to_string(j));
+            }
 
+            auto start = std::chrono::high_resolution_clock::now();
+
+            g.addVertex("V" + std::to_string(current_size));
+
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::micro> duration = end - start;
+
+            total_time_us += duration.count();
+        }
+
+        double avg_time = total_time_us / repeats;
+        outfile << current_size << " " << avg_time << "\n";
+    }
+
+    outfile.close();
     return 0;
 }
--- tests/test_graph.cpp
+++ tests/test_graph.cpp
@@ -1,7 +1,2 @@
 #include <gtest/gtest.h>
 #include "../headers/graph.hpp"
-
-TEST(Compare, Equals)
-{
-    EXPECT_EQ(1, 1);
-}
